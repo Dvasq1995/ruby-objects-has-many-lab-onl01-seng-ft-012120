27e4d323d0cef62ae5179c864eca95ba55c64f8a
Diego Vasquez <botellongo69@outlook.com>
2020-02-04 19:56:18 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,8 +15,8 @@ class Author
     post.author = self
   end
   
-  def add_post_by_title(post_name)
-    post = Post.new(post_name)
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
     add_post(post)
   end
   
